69245ab5f3ae2f902ff8ec4a0370d1b3dc9a8d78
PRO <54608551+PRO-2684@users.noreply.github.com>
2020-02-08 14:38:27 +0800

    Update android.py

--- android.py
+++ android.py
@@ -1,10 +1,14 @@
-"""This module is created by PRO, aiming at making it easier for termux users to use android API.
+"""This module is created by PRO-2684, aiming at making it easier for termux users to use android API via python.
 Please install 'Termux: API' app on your phone and 'termux-api' package on termux before using this module.
-For more help, use help(xxx)."""
+For more help, use help(xxx).
+此模块由PRO-2684创建，旨在让termux用户更方便地在python中使用Android API。
+请先安装与Termux同来源的'Termux: API' app，并在Termux中安装termux-api包后再使用。
+更多帮助请使用 help(xxx)。
+"""
 
 from os import popen
 class MediaPlayer:
-    """Introduction: Play media via python on termux."""
+    """Introduction: Used to play media."""
     def play(path=''):
         """Takes one or zero argument.
         If an argument is given, it will play the media at specified path.
